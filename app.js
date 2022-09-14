3107c9f02410a422828ace0c2fa2ed42eaaf0996
remlgt <remi.loigerot41@gmail.com>
2022-09-14 14:54:19 +0200

    A2

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ const listKeyCode = keys.map(element => element.dataset.key);
 
 // console.log(listKeyCode);
 
+document.addEventListener('keydown',(e) => {
+    // console.log(e);
+    const value = e.keyCode.toString();
+    // console.log(value, typeof value);
+});
+
+document.addEventListener('click',(e) => {
+    // console.log(e);
+    const clickValue = e.target.dataset.key;
+    //  console.log(clickValue, typeof clickValue);
+}); 
+
+
